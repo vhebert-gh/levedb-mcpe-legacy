aac9314ae2800a203e1be5102fa00ca972f3449c
Tommaso Checchi <tommaso.checchi1@gmail.com>
2017-08-02 15:59:46 -0700

    Added a new ZlibCompressorRaw which isn't directly compatible with the old ZlibCompressor; it skips the CRC check and a few fields though.

--- db/zlib_compressor.cc
+++ db/zlib_compressor.cc
@@ -7,7 +7,7 @@
 
 namespace leveldb {
 
-	void ZlibCompressor::compressImpl(const char* input, size_t length, ::std::string& buffer) const
+	void ZlibCompressorBase::compressImpl(const char* input, size_t length, ::std::string& buffer) const
 	{
 		const size_t BUFSIZE = 128 * 1024;
 		unsigned char temp_buffer[BUFSIZE];
@@ -23,8 +23,9 @@ namespace leveldb {
 		strm.next_out = temp_buffer;
 		strm.avail_out = BUFSIZE;
 
-		deflateInit(&strm, compressionLevel);
-
+		auto res = deflateInit2(&strm, compressionLevel, Z_DEFLATED, _window(), 8, Z_DEFAULT_STRATEGY);
+		assert(res == Z_OK);
+		
 		int deflate_res = Z_OK;
 		while (strm.avail_in != 0)
 		{
@@ -32,7 +33,7 @@ namespace leveldb {
 			assert(res == Z_OK);
 			if (strm.avail_out == 0)
 			{
-				buffer.insert(buffer.end(), temp_buffer, temp_buffer + BUFSIZE);
+				buffer.append(temp_buffer, temp_buffer + BUFSIZE);
 				strm.next_out = temp_buffer;
 				strm.avail_out = BUFSIZE;
 			}
@@ -42,7 +43,7 @@ namespace leveldb {
 		{
 			if (strm.avail_out == 0)
 			{
-				buffer.insert(buffer.end(), temp_buffer, temp_buffer + BUFSIZE);
+				buffer.append(temp_buffer, temp_buffer + BUFSIZE);
 				strm.next_out = temp_buffer;
 				strm.avail_out = BUFSIZE;
 			}
@@ -50,11 +51,11 @@ namespace leveldb {
 		}
 
 		assert(deflate_res == Z_STREAM_END);
-		buffer.insert(buffer.end(), temp_buffer, temp_buffer + BUFSIZE - strm.avail_out);
+		buffer.append(temp_buffer, temp_buffer + BUFSIZE - strm.avail_out);
 		deflateEnd(&strm);
 	}
 
-	int ZlibCompressor::inflate(const char* input, size_t length, ::std::string &output) {
+	int ZlibCompressorBase::inflate(const char* input, size_t length, ::std::string &output) const {
 		const int CHUNK = 64 * 1024;
 
 		int ret;
@@ -68,7 +69,9 @@ namespace leveldb {
 		strm.opaque = Z_NULL;
 		strm.avail_in = (uint32_t)length;
 		strm.next_in = (Bytef*)input;
-		ret = inflateInit(&strm);
+
+		ret = inflateInit2(&strm, _window());
+
 		if (ret != Z_OK)
 		{
 			return ret;
@@ -106,10 +109,14 @@ namespace leveldb {
 		return ret == Z_STREAM_END ? Z_OK : Z_DATA_ERROR;
 	}
 
-	bool ZlibCompressor::decompress(const char* input, size_t length, ::std::string &output) const {
-		return ZlibCompressor::inflate(input, length, output) == Z_OK;
+	bool ZlibCompressorBase::decompress(const char* input, size_t length, ::std::string &output) const {
+		return inflate(input, length, output) == Z_OK;
 	}
 		
+	int ZlibCompressorBase::_window() const {
+		return raw ? -15 : 15;
+	}
+
 }
 
 #endif
--- include/leveldb/zlib_compressor.h
+++ include/leveldb/zlib_compressor.h
@@ -5,22 +5,22 @@
 
 namespace leveldb {
 
-	class DLLX ZlibCompressor : public Compressor 
+	class DLLX ZlibCompressorBase : public Compressor 
 	{
 	public:
-		static const int SERIALIZE_ID = 2;
-
-		static int inflate(const char* input, size_t length, ::std::string &output);
+		int inflate(const char* input, size_t length, ::std::string &output) const;
 
 		const int compressionLevel;
+		const bool raw;
         
-        virtual ~ZlibCompressor() {
+        virtual ~ZlibCompressorBase() {
             
         }
 
-		ZlibCompressor(int compressionLevel = -1) :
-			Compressor(SERIALIZE_ID),
-			compressionLevel(compressionLevel)
+		ZlibCompressorBase(char uniqueCompressionID, int compressionLevel, bool raw) :
+			Compressor(uniqueCompressionID),
+			compressionLevel(compressionLevel),
+			raw(raw)
 		{
 			assert(compressionLevel >= -1 && compressionLevel <= 9);
 		}
@@ -31,5 +31,27 @@ namespace leveldb {
 
 	private:
 
+		int _window() const;
+
+	};
+
+	class ZlibCompressor : public ZlibCompressorBase {
+	public:
+		static const int SERIALIZE_ID = 2;
+
+		ZlibCompressor(int compressionLevel) :
+			ZlibCompressorBase(SERIALIZE_ID, compressionLevel, false) {
+
+		}
+	};
+
+	class ZlibCompressorRaw : public ZlibCompressorBase {
+	public:
+		static const int SERIALIZE_ID = 4;
+
+		ZlibCompressorRaw(int compressionLevel) :
+			ZlibCompressorBase(SERIALIZE_ID, compressionLevel, true) {
+
+		}
 	};
 }
